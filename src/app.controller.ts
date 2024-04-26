bb63fa095912a049e9eb3777d4eff875ad9c7141
ohddang <ohddang509@gmail.com>
2024-04-26 22:31:25 +0900

    hotfix: options 전역 처리

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Options, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
 
 @Controller('chat')
 export class AppController {
@@ -8,5 +9,19 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Options('*')
+  options(@Res() response: Response): void {
+    response.header('Access-Control-Allow-Origin', '*');
+    response.header(
+      'Access-Control-Allow-Methods',
+      'GET,PUT,POST,DELETE,OPTIONS',
+    );
+    response.header(
+      'Access-Control-Allow-Headers',
+      'Content-Type, Authorization',
+    );
+    response.json();
   }
 }
